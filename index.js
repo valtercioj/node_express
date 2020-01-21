d0f0341f8054b6b2e75503687c6b52c337210d47
valtercioj <53585971+valtercioj@users.noreply.github.com>
2020-01-21 11:38:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var path = __dirname + '/views/';
+var PORT = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
   res.sendFile(path + "index.html");
